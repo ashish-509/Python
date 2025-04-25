b0c101072aa07207542ca2ab282e0ce5dfb7e193
ashish-509 <ashishkandel509@gmail.com>
2025-04-25 23:15:33 +0545

    commit

--- Python_new/mutable_immutable.py
+++
@@ -1,7 +0,0 @@
-# Mutable :
-    # - can be changed
-    # - Eg :list, set, dict, array, bytearray
-
-# Immutable :
-    # - cannot be changed
-    # - Eg :int, float, bool, str, tuple, frozenset, bytes
