d60ab236c5e1b0ef876729a31068112ee8944ecc
ashish-509 <ashish.078bei006@tcioe.edu.np>
2026-05-03 11:35:11 +0545

    Reorganized files and added new content

--- 6_virtual_env.py
+++
@@ -1,3 +0,0 @@
-# pip install virtualenv
-
-# pip list > requirements.txt       # inside virtual environment to list all the requirements in requirements.txt file
--- fileCopyContent.py
+++
@@ -1,6 +0,0 @@
-# Copying contents from one file to another
-
-with open ('fileSource.txt', 'r') as sourceFile:
-    with open ('fileDestination.txt', 'w') as destinationFile:
-        for line in sourceFile:
-            destinationFile.write(line)
