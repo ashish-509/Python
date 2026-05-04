f23af045ee3fa757f322362a79a7e3467a63ffee
ashish-509 <ashish.078bei006@tcioe.edu.np>
2026-05-04 13:09:45 +0545

    find first non-repeating character in a string

--- Python/Python_Practice/4_strings.py
+++ Python/Python_Practice/4_strings.py
@@ -1,1 +1,29 @@
 # Find the first non-repeating character in a string.
+
+
+def first_non_repeating_character(s):
+    char_count = {}
+    
+    for char in s:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+
+    for char in s:
+        if char_count[char] == 1:
+            return char
+
+    return None 
+
+
+# Example usage:
+
+input_string = input("Enter a string : ")
+result = first_non_repeating_character(input_string)
+
+if result:
+    print(f"The first non-repeating character in '{input_string}' is: '{result}'")
+else:    
+    print(f"There are no non-repeating characters in '{input_string}'.")                       
+
