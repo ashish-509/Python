b26fc38790d258b6fb91c8ae22130ff2394484ce
ashish-509 <ashish.078bei006@tcioe.edu.np>
2026-05-04 13:04:02 +0545

    2nd largest no. in a list

--- Python/Python_Practice/3_2ndLargestNumber.py
+++ Python/Python_Practice/3_2ndLargestNumber.py
@@ -1,3 +1,28 @@
 # Given a list of integers, return:
     # second largest number
     # without using built-in sort
+
+
+def second_largest (nums):
+
+    if len(nums)<2:
+        return None
+
+    largest = nums[0]
+    second = None
+
+    for i in range(1, len(nums)):
+        if nums[i] > largest:
+            second = largest
+            largest = nums[i]
+        elif second is None or nums[i] > second:
+            second = nums[i]
+
+    return second
+
+# Test Cases
+print(second_largest([1, 2, 3, 4, 5]))
+print(second_largest([5, 4, 3, 2, 1]))
+print(second_largest([1, 1, 1, 1, 1]))
+print(second_largest([1]))
+print(second_largest([]))   
