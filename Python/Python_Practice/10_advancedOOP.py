1627788efc7e1afb710088e63745b4a5e39e0365
ashish-509 <ashish.078bei006@tcioe.edu.np>
2026-05-05 21:15:34 +0545

    Advanced OOP - Child  classes

--- Python/Python_Practice/10_advancedOOP.py
+++ Python/Python_Practice/10_advancedOOP.py
@@ -13,3 +13,22 @@ class Model:
         raise NotImplementedError("Subclasses must implement this method")      
 
     
+class LinearRegression(Model):
+    def train(self, X, y):
+        print("Training Linear Regression model with data:", X, y)
+
+    def predict(self, X):
+        print("Predicting with Linear Regression model for data:", X)
+        return [0] * len(X)  # Dummy prediction
+
+
+class DecisionTree(Model):
+    def train(self, X, y):
+        print("Training Decision Tree model with data:", X, y)
+
+    def predict(self, X):
+        print("Predicting with Decision Tree model for data:", X)
+        return [1] * len(X)  # Dummy prediction
+
+
+
