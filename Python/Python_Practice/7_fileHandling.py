b1ee884228e900bb5f028ea2e3605a59e69a24af
ashish-509 <ashish.078bei006@tcioe.edu.np>
2026-05-05 21:01:24 +0545

    file reading and return word freq. dictionary

--- Python/Python_Practice/7_fileHandling.py
+++ Python/Python_Practice/7_fileHandling.py
@@ -1,3 +1,24 @@
 # Read a text file and return:
     # word frequency dictionary
     # top 5 most common words
+
+
+def word_frequency(file_path):
+    frequency = {}
+    with open(file_path, 'r') as file:
+        for line in file:
+            words = line.split()
+            for word in words:
+                word = word.lower().strip('.,!?";()')
+                frequency[word] = frequency.get(word, 0) + 1
+    return frequency
+
+def top_5_words(frequency):
+    return sorted(frequency.items(), key=lambda x: x[1], reverse=True)[:5]
+
+
+# Example usage
+file_path = '7_readFile.txt'  
+frequency = word_frequency(file_path)
+print("Word Frequency Dictionary:", frequency)  
+print("Top 5 Most Common Words:", top_5_words(frequency))
