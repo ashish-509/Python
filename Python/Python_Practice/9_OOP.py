ae9a07f37b545ec2eb5c8ba01aee2230866f92b6
ashish-509 <ashish.078bei006@tcioe.edu.np>
2026-05-05 21:07:05 +0545

    OOP - BankAccount class

--- Python/Python_Practice/9_OOP.py
+++ Python/Python_Practice/9_OOP.py
@@ -3,3 +3,28 @@
     # withdraw
     # balance check
     # error handling for insufficient balance
+
+
+
+class BankAccount:  
+    def __init__(self, balance=0):
+        self.balance = balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print(f"Deposited: ${amount}. New balance: ${self.balance}.")
+        else:
+            print("Deposit amount must be positive.")
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient balance.")
+        elif amount <= 0:
+            print("Withdrawal amount must be positive.")
+        else:
+            self.balance -= amount
+            print(f"Withdrew: ${amount}. New balance: ${self.balance}.")
+
+    def check_balance(self):
+        print(f"Current balance: ${self.balance}.")
