8b93d968b3952ce238ed5c41a463d16286a523a1
ashish-509 <ashish.078bei006@tcioe.edu.np>
2026-05-05 21:14:55 +0545

    Advanced OOP - Base class

--- Python/Python_Practice/10_advancedOOP.py
+++ Python/Python_Practice/10_advancedOOP.py
@@ -2,3 +2,14 @@
     # Base class: Model
     # Derived classes: LinearRegression, DecisionTree
     # Add train() and predict() methods
+
+
+
+class Model:
+    def train(self, X, y):
+        raise NotImplementedError("Subclasses must implement this method")
+
+    def predict(self, X):
+        raise NotImplementedError("Subclasses must implement this method")      
+
+    
