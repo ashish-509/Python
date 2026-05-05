36d0a365dbd3410f6e0dd0e0d3c146eb4552045d
ashish-509 <ashish.078bei006@tcioe.edu.np>
2026-05-05 21:15:56 +0545

    Advanced OOP

--- Python/Python_Practice/10_advancedOOP.py
+++ Python/Python_Practice/10_advancedOOP.py
@@ -32,3 +32,22 @@ class DecisionTree(Model):
 
 
 
+# Example usage:
+X_train = [[1, 2], [3, 4], [5, 6]]
+y_train = [0, 1, 0] 
+X_test = [[2, 3], [4, 5]]
+
+# Create model instances
+lr_model = LinearRegression()
+dt_model = DecisionTree()
+
+# Train models
+lr_model.train(X_train, y_train)
+dt_model.train(X_train, y_train)
+
+# Make predictions
+lr_predictions = lr_model.predict(X_test)
+dt_predictions = dt_model.predict(X_test)
+
+print("Linear Regression Predictions:", lr_predictions)
+print("Decision Tree Predictions:", dt_predictions)
