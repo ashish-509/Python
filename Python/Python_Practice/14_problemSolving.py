d948480047ee9c396ea0a11991c1cf9b96552f29
ashish-509 <ashish.078bei006@tcioe.edu.np>
2026-05-06 20:36:37 +0545

    train-test split

--- Python/Python_Practice/14_problemSolving.py
+++ Python/Python_Practice/14_problemSolving.py
@@ -18,6 +18,21 @@ def min_max_scaling(scores):
     return scaled_scores
 
 
+def train_test_split(scores, test_size=0.2):
+    np.random.shuffle(scores)
+    split_index = int(len(scores) * (1 - test_size))
+    train_scores = scores[:split_index]
+    test_scores = scores[split_index:]
+    return train_scores, test_scores
 
+
+# Example usage:
+student_scores = np.array([85, 90, 78, 92, 88, 95, 80, 82])
+normalized_scores = min_max_scaling(student_scores)
+print("Normalized Scores:", normalized_scores)
+
+train_scores, test_scores = train_test_split(normalized_scores, test_size=0.2)
+print("Train Scores:", train_scores)
+print("Test Scores:", test_scores)
 
 
