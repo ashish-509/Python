86adcde13ed265f0f05e75284899129b4be4c85f
ashish-509 <ashish.078bei006@tcioe.edu.np>
2026-05-06 19:35:31 +0545

    custom dot product

--- Python/Python_Practice/12_numpy.py
+++ Python/Python_Practice/12_numpy.py
@@ -1,3 +1,22 @@
 # Without using built-in NumPy functions:
     #     implement dot product
     #     Then compare with NumPy’s result.
+
+
+
+def dot_product(vec1, vec2):
+    if len(vec1) != len(vec2):
+        raise ValueError("Vectors must be of the same length")
+    
+    result = 0
+    for i in range(len(vec1)):
+        result += vec1[i] * vec2[i]
+    
+    return result
+
+
+# Example usage:
+vec1 = [1, 2, 3]
+vec2 = [4, 5, 6]
+custom_dot = dot_product(vec1, vec2)
+print("Custom Dot Product:", custom_dot)
