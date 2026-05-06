a59be3e64d9c1dcb5c334100c7775399d407fbd8
ashish-509 <ashish.078bei006@tcioe.edu.np>
2026-05-06 20:36:06 +0545

    min-max scaling

--- Python/Python_Practice/14_problemSolving.py
+++ Python/Python_Practice/14_problemSolving.py
@@ -1,3 +1,23 @@
 # Given a dataset of student scores:
     # normalize values (min-max scaling)
     # split into train/test (80/20)
+
+
+
+import numpy as np
+
+
+def min_max_scaling(scores):
+    min_score = np.min(scores)
+    max_score = np.max(scores)
+    
+    if max_score - min_score == 0:
+        return np.zeros_like(scores)  # Avoid division by zero
+    
+    scaled_scores = (scores - min_score) / (max_score - min_score)
+    return scaled_scores
+
+
+
+
+
